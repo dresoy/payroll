443f2a9feb9c6aa1ee008ac7023c021b2ad0a8b1
Dresoy Ravelo <dresoy@hotmail.com>
2018-04-08 22:54:26 -0300

    Reading the XLS file

---
+++ Payroll/Payroll/Controllers/UploadExcelController.cs
@@ -0,0 +1,61 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Payroll.Controllers
+{
+    public class UploadExcelController : Controller
+    {
+        //// GET: UploadExcell
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public ActionResult UploadExcelFile()
+        {
+            ViewBag.loading = false;
+            ViewBag.hasError = false;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UploadExcelFile(HttpPostedFileBase file)
+        {
+            ViewBag.loading = true;
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    //string _FileName = Path.GetFileName(file.FileName);
+                    //string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    //file.SaveAs(_path);
+                    DataSet ExcelDS = await Utilities.ExcelManager.ExcelReaderAsync(file.InputStream);
+
+
+
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                ViewBag.hasError = true;
+                ViewBag.loading = false;
+                return View();
+            }
+        }
+
+
+
+    }
+}
---
+++ Payroll/Payroll/Utilities/ExcelManager.cs
@@ -0,0 +1,45 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Payroll.Utilities
+{
+    public static class ExcelManager
+    {
+        public static async Task<DataSet> ExcelReaderAsync(Stream file)
+        {
+            //using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            using (var stream = file)
+            {
+
+                // Auto-detect format, supports:
+                //  - Binary Excel files (2.0-2003 format; *.xls)
+                //  - OpenXml Excel files (2007 format; *.xlsx)
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+
+                    //// Choose one of either 1 or 2:
+
+                    //// 1. Use the reader methods
+                    //do
+                    //{
+                    //    while (reader.Read())
+                    //    {
+                    //        // reader.GetDouble(0);
+                    //    }
+                    //} while (reader.NextResult());
+
+                    // 2. Use the AsDataSet extension method
+                    DataSet result = await Task.Run(()=> reader.AsDataSet());
+                    return result;
+                    // The result of each spreadsheet is in result.Tables
+                }
+            }
+        }
+    }
+}
